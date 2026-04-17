d7bfe8a1f39f77f6699f9ef9e2c5d679f9ebb786
anirudhburman <anirudh.burman.micro@gmail.com>
2026-04-17 22:27:50 +0530

    Added config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: 'export', // Required for GitHub Pages
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
+  },
+  // If your URL is username.github.io/repo-name, add:
+  basePath: '/sr-enterprises',
 };
 
 export default nextConfig;
